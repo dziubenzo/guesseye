0f880f569d15ebba20b48beedefca24349f8964e
dziubenzo <dziubenzo@gmail.com>
2025-06-11 15:36:13 +0200

    Build proper header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,64 +1,26 @@
 'use client';
 
 import AuthModal from '@/components/AuthModal';
+import HeaderMenu from '@/components/HeaderMenu';
 import { Button } from '@/components/ui/button';
-import { signOut, useSession } from '@/lib/auth-client';
-import { useGameStore } from '@/lib/game-store';
+import { useSession } from '@/lib/auth-client';
 import { Dialog, DialogTrigger } from '@radix-ui/react-dialog';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 export default function Header() {
   const { data } = useSession();
-  const router = useRouter();
-  const { resetState } = useGameStore();
 
-  async function logOut() {
-    await signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          resetState();
-          router.push('/');
-        },
-      },
-    });
-  }
+  if (data) return <HeaderMenu username={data.user.name} />;
 
   return (
-    <header className="grid grid-flow-col lg:grid-cols-5 text-center items-center">
-      <p>
-        <Button className="cursor-pointer" variant="link" asChild>
-          <Link href="/stats">Stats</Link>
-        </Button>
-      </p>
-      <p>
-        <Button className="cursor-pointer" variant="link" asChild>
-          <Link href="/leaderboard">Leaderboard</Link>
-        </Button>
-      </p>
-      <p>
-        <Button className="cursor-pointer" variant="link" asChild>
-          <Link href="/history">History</Link>
-        </Button>
-      </p>
-      <p>
-        <Button className="cursor-pointer" variant="link" asChild>
-          <Link href="/official">Official Games</Link>
-        </Button>
-      </p>
-      {data && (
-        <Button className="cursor-pointer w-full" onClick={logOut}>
-          Log Out
-        </Button>
-      )}
-      {!data && (
+    <header className="grid grid-flow-col lg:grid-cols-4 text-center items-center justify-center">
+      <div className="col-start-4">
         <Dialog>
           <DialogTrigger asChild>
             <Button className="cursor-pointer w-full">Log In/Sign Up</Button>
           </DialogTrigger>
           <AuthModal />
         </Dialog>
-      )}
+      </div>
     </header>
   );
 }
---
+++ src/components/HeaderMenu.tsx
@@ -0,0 +1,245 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu';
+import { signOut } from '@/lib/auth-client';
+import { useGameStore } from '@/lib/game-store';
+import {
+  ArrowBigLeft,
+  ArrowBigUp,
+  ChartNoAxesCombined,
+  ChartSpline,
+  DatabaseZap,
+  Dices,
+  GalleryHorizontalEnd,
+  LogOut,
+  Presentation,
+  Settings,
+  Shuffle,
+  Table,
+} from 'lucide-react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+const CUSTOM_TRIGGER_CLASS =
+  'bg-primary text-primary-foreground data-[state=open]:bg-primary data-[state=open]:text-primary-foreground data-[state=open]:hover:bg-primary data-[state=open]:hover:text-primary-foreground data-[state=closed]:hover:bg-primary data-[state=closed]:hover:text-primary-foreground data-[state=closed]:bg-primary data-[state=closed]:text-primary-foreground hover:bg-primary hover:text-primary-foreground focus-visible:bg-primary focus:bg-primary focus-visible:text-primary-foreground focus:text-primary-foreground data-[state=open]:focus:bg-primary min-w-full w-full sm:min-h-10 sm:h-10';
+
+const CUSTOM_MENU_CLASS = 'grid w-full sm:w-[420px] gap-2 sm:gap-4';
+
+type HeaderMenuProps = {
+  username: string;
+};
+
+export default function HeaderMenu({ username }: HeaderMenuProps) {
+  const router = useRouter();
+  const { resetState } = useGameStore();
+
+  async function logOut() {
+    await signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          resetState();
+          router.push('/');
+        },
+      },
+    });
+  }
+
+  return (
+    <header className="ml-auto mr-auto">
+      <NavigationMenu viewport={true} className="z-2">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className={CUSTOM_TRIGGER_CLASS}>
+              <div className="sm:hidden">
+                <ChartSpline />
+              </div>
+              <div className="hidden sm:block">Stats</div>
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className={CUSTOM_MENU_CLASS}>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="/stats">
+                      <div className="flex gap-4 items-center">
+                        <ChartNoAxesCombined className="min-w-4 max-w-4" />
+                        <div>
+                          <div className="font-medium">Game Stats</div>
+                          <div className="text-muted-foreground">
+                            Discover your game stats as well as global game
+                            stats.
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="/database">
+                      <div className="flex gap-4 items-center">
+                        <DatabaseZap className="min-w-4 max-w-4" />
+                        <div>
+                          <div className="font-medium">Database Stats</div>
+                          <div className="text-muted-foreground">
+                            Take a look at database stats.
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className={CUSTOM_TRIGGER_CLASS}>
+              <div className="sm:hidden">
+                <Table />
+              </div>
+              <div className="hidden sm:block">Leaderboard</div>
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className={CUSTOM_MENU_CLASS}>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="/leaderboard">
+                      <div className="flex gap-4 items-center">
+                        <Presentation className="min-w-4 max-w-4" />
+                        <div>
+                          <div className="font-medium">Leaderboard</div>
+                          <div className="text-muted-foreground">
+                            Browse the leaderboard.
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="/history">
+                      <div className="flex gap-4 items-center">
+                        <GalleryHorizontalEnd className="min-w-4 max-w-4" />
+                        <div>
+                          <div className="font-medium">
+                            Official Games History
+                          </div>
+                          <div className="text-muted-foreground">
+                            Discover winners of previous official games.
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className={CUSTOM_TRIGGER_CLASS}>
+              <div className="sm:hidden">
+                <Dices />
+              </div>
+              <div className="hidden sm:block">Games</div>
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className={CUSTOM_MENU_CLASS}>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="/">
+                      <div className="flex gap-4 items-center">
+                        <ArrowBigUp className="min-w-4 max-w-4" />
+                        <div>
+                          <div className="font-medium">
+                            Current Official Game
+                          </div>
+                          <div className="text-muted-foreground">
+                            Play the current official game.
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="/official">
+                      <div className="flex gap-4 items-center">
+                        <ArrowBigLeft className="min-w-4 max-w-4" />
+                        <div>
+                          <div className="font-medium">
+                            Previous Official Games
+                          </div>
+                          <div className="text-muted-foreground">
+                            Browse, play or resume all previous official games.
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="/random">
+                      <div className="flex gap-4 items-center">
+                        <Shuffle className="min-w-4 max-w-4" />
+                        <div>
+                          <div className="font-medium">Random Game</div>
+                          <div className="text-muted-foreground">
+                            Play or resume a random game.
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className={CUSTOM_TRIGGER_CLASS}>
+              <div className="sm:hidden">
+                <Settings />
+              </div>
+              <div className="hidden sm:block">My Account</div>
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className={CUSTOM_MENU_CLASS}>
+                <li>
+                  <p className="text-sm text-center">
+                    Hi, <span className="font-medium">{username}</span>!
+                  </p>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="/settings">
+                      <div className="flex gap-4 items-center">
+                        <Settings className="min-w-4 max-w-4" />
+                        <div>
+                          <div className="font-medium">Settings</div>
+                          <div className="text-muted-foreground">
+                            Change your name or delete the account.
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Button
+                      className="cursor-pointer w-full flex flex-row gap-2 items-center justify-end"
+                      variant={'ghost'}
+                      onClick={logOut}
+                    >
+                      <LogOut />
+                      <p>Log Out</p>
+                    </Button>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+}
