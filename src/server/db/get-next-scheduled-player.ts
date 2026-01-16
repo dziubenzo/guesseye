0e930d50c414ccb2929d308149c5ab02bcf25b2f
dziubenzo <dziubenzo@gmail.com>
2026-01-16 19:01:52 +0100

    Make sure next official player is the right one

--- src/server/db/get-next-scheduled-player.ts
+++ src/server/db/get-next-scheduled-player.ts
@@ -3,25 +3,26 @@
 import type { ErrorObject, NextScheduledPlayer, Schedule } from '@/lib/types';
 import { db } from '@/server/db/index';
 import { schedule } from '@/server/db/schema';
-import { gte } from 'drizzle-orm';
+import { eq } from 'drizzle-orm';
 
 export const getNextScheduledPlayer = async (
   currentPlayerEndDate: Schedule['endDate']
 ) => {
-  const nextScheduledPlayer = await db.query.schedule.findFirst({
-    columns: { startDate: true },
-    where: gte(schedule.startDate, currentPlayerEndDate),
-    with: {
-      playerToFind: {
-        columns: { difficulty: true },
+  const nextScheduledPlayer: NextScheduledPlayer | undefined =
+    await db.query.schedule.findFirst({
+      columns: { startDate: true },
+      where: eq(schedule.startDate, currentPlayerEndDate),
+      with: {
+        playerToFind: {
+          columns: { difficulty: true },
+        },
       },
-    },
-  });
+    });
 
   if (!nextScheduledPlayer) {
     const error: ErrorObject = { error: 'No next darts player scheduled.' };
     return error;
   }
 
-  return nextScheduledPlayer as NextScheduledPlayer;
+  return nextScheduledPlayer;
 };
