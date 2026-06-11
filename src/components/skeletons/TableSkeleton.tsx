11c3151a9a4fa744db4d76e34cd7bb64ad5b1959
dziubenzo <dziubenzo@gmail.com>
2026-06-11 16:26:54 +0200

    Make TableSkeleton look friendlier

--- src/components/skeletons/TableSkeleton.tsx
+++ src/components/skeletons/TableSkeleton.tsx
@@ -1,4 +1,5 @@
 import { Skeleton } from '@/components/ui/skeleton';
+import { cn } from '@/lib/utils';
 
 type TableSkeletonProps = {
   rows?: number;
@@ -7,12 +8,27 @@ type TableSkeletonProps = {
 export default function TableSkeleton({ rows }: TableSkeletonProps) {
   return (
     <div className="flex flex-col w-full gap-2">
-      <Skeleton className="h-10" />
+      <Skeleton className="h-10 dark:bg-gray-500 bg-gray-400" />
       {Array.from({ length: rows ? rows : 10 }).map((_, index) => (
         <div className="flex gap-2" key={index}>
-          <Skeleton className="h-14 flex-1" />
-          <Skeleton className="h-14 w-24" />
-          <Skeleton className="h-14 w-20" />
+          <Skeleton
+            className={cn(
+              'h-14 flex-1',
+              index % 2 === 1 && 'dark:bg-gray-500 bg-gray-400'
+            )}
+          />
+          <Skeleton
+            className={cn(
+              'h-14 w-24',
+              index % 2 === 1 && 'dark:bg-gray-500 bg-gray-400'
+            )}
+          />
+          <Skeleton
+            className={cn(
+              'h-14 w-20',
+              index % 2 === 1 && 'dark:bg-gray-500 bg-gray-400'
+            )}
+          />
         </div>
       ))}
     </div>
