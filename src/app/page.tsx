a2a3654a29829ce22f447d0dfe3a3c86adcecc49
dziubenzo <dziubenzo@gmail.com>
2026-06-23 15:11:57 +0200

    Use Cache Components for Current Game page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,17 @@
-import GamePage from '@/components/GamePage';
+import CurrentGame from '@/components/CurrentGame';
 import GamePageSkeleton from '@/components/skeletons/GamePageSkeletons';
-import { auth } from '@/lib/auth';
-import { getOfficialGame } from '@/server/db/get-official-game';
-import { getPlayerFullNames } from '@/server/db/get-player-full-names';
-import { getRandomGame } from '@/server/db/get-random-game';
+import { getSession } from '@/server/utils';
 import type { Metadata } from 'next';
-import { headers } from 'next/headers';
 import { Suspense } from 'react';
 
 export const metadata: Metadata = { title: 'GuessEye' };
 
-export default async function CurrentGame() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-  const namesPromise = getPlayerFullNames();
+export default async function CurrentGamePage() {
+  const sessionPromise = getSession();
 
-  if (session) {
-    const gamePromise = getOfficialGame();
-
-    return (
-      <Suspense fallback={<GamePageSkeleton isOfficialGame />}>
-        <GamePage
-          gameMode="official"
-          gamePromise={gamePromise}
-          namesPromise={namesPromise}
-          userId={session.user.id}
-        />
-      </Suspense>
-    );
-  } else {
-    const gamePromise = getRandomGame({ isGuest: true });
-
-    return (
-      <Suspense fallback={<GamePageSkeleton />}>
-        <GamePage
-          gameMode="random"
-          gamePromise={gamePromise}
-          namesPromise={namesPromise}
-        />
-      </Suspense>
-    );
-  }
+  return (
+    <Suspense fallback={<GamePageSkeleton isOfficialGame/>}>
+      <CurrentGame sessionPromise={sessionPromise} />
+    </Suspense>
+  );
 }
---
+++ src/components/CurrentGame.tsx
@@ -0,0 +1,43 @@
+import GamePage from '@/components/GamePage';
+import GamePageSkeleton from '@/components/skeletons/GamePageSkeletons';
+import { type Session } from '@/lib/auth';
+import { getOfficialGame } from '@/server/db/get-official-game';
+import { getPlayerFullNames } from '@/server/db/get-player-full-names';
+import { getRandomGame } from '@/server/db/get-random-game';
+import { Suspense, use } from 'react';
+
+type CurrentGameProps = {
+  sessionPromise: Promise<Session | null>;
+};
+
+export default function CurrentGame({ sessionPromise }: CurrentGameProps) {
+  const session = use(sessionPromise);
+  const namesPromise = getPlayerFullNames();
+
+  if (session) {
+    const gamePromise = getOfficialGame();
+
+    return (
+      <Suspense fallback={<GamePageSkeleton isOfficialGame />}>
+        <GamePage
+          gameMode="official"
+          gamePromise={gamePromise}
+          namesPromise={namesPromise}
+          userId={session.user.id}
+        />
+      </Suspense>
+    );
+  } else {
+    const gamePromise = getRandomGame({ isGuest: true });
+
+    return (
+      <Suspense fallback={<GamePageSkeleton />}>
+        <GamePage
+          gameMode="random"
+          gamePromise={gamePromise}
+          namesPromise={namesPromise}
+        />
+      </Suspense>
+    );
+  }
+}
